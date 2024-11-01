5fa24690b15661c4b5c527e5acb0009fd3764790
Hiro3328 <arturhbaron@gmail.com>
2024-11-01 20:22:44 -0300

    making code more readable

--- src/hiroMethod.py
+++ src/hiroMethod.py
@@ -4,17 +4,27 @@ Encrypts or Decrypts a string, based on a given key, default is 3218
 
 Made By: Hiro3328 /#/ Current Rev: 3.3
 """
+from string import ascii_letters as characters
+print(characters)
 
 def encrypt(message: str, key = 3218):
     """Encrypts a string, based on a given key, default is 3218\n    
     Made By: Hiro3328"""
-    characters = ["e","a","r","i","o","t","n","s","l","c","u","d","p","m","h","g","b","f","y","w","k","v","x","z","j","q"]
-    return ''.join(characters[(characters.index(char.lower()) + int(key*pos))%26] if char.lower() in characters else char for pos, char in enumerate(message))
+    return ''.join(
+        characters[
+        (characters.index(char.lower()) + int(key*pos))%26
+    ] if char.lower() in characters 
+        else char 
+    for pos, char in enumerate(message))
 
 def decrypt(message: str, key = 3218):
     """Decrypts a string, based on a given key, default is 3218\n    
     Made By: Hiro3328"""                                
-    characters = ["e","a","r","i","o","t","n","s","l","c","u","d","p","m","h","g","b","f","y","w","k","v","x","z","j","q"]
-    return ''.join(characters[(characters.index(char.lower()) - int(key*pos))%26] if char.lower() in characters else char for pos, char in enumerate(message))
+    return ''.join(
+        characters[
+        (characters.index(char.lower()) - int(key*pos))%26
+    ] if char.lower() in characters 
+        else char 
+    for pos, char in enumerate(message))
 
 
